ce74159c65a005241338009ce6c5eec0e997325b
grou-p-6 <imageuploader79@gmail.com>
2023-10-07 23:20:10 +0530

    Init commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, render_template, request, Response, url_for
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+uploaded_image_data = []
+
+# route to index.html
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# route to upload.html
+
+
+@app.route('/api/upload', methods=['POST'])
+def upload_image():
+    print("Reached upload_image")
+    global uploaded_image_data
+
+    # Check if the post request has the file part.
+    if 'image' not in request.files:
+        return jsonify({"error": "No image file provided"}), 400
+
+    image = request.files['image']
+    if image.filename == '':
+        return jsonify({"error": "No selected image"}), 400
+
+    if image:
+        # Save image data to variable.
+        image_content = image.read()
+        uploaded_image_data.append(image_content)
+        # Return the url of the uploaded image.
+        url = url_for('get_image', image_id=len(
+            uploaded_image_data) - 1, _external=True)
+        return jsonify({"message": "Image uploaded successfully", "len": len(uploaded_image_data), "url": url}), 200
+
+# route to get image by id
+
+
+@app.route('/api/images/<int:image_id>')
+def get_image(image_id):
+    if 0 <= image_id < len(uploaded_image_data):
+        return Response(uploaded_image_data[image_id], content_type='image/jpeg')
+    else:
+        return jsonify({"message": "Image not found"}), 404
+
+# route to get total image count
+
+
+@app.route('/api/image_count')
+def get_image_count():
+    return jsonify({"total_images": len(uploaded_image_data)})
+
+# route to get all images
+
+
+@app.route('/api/images')
+def get_all_images():
+    image_urls = [url_for('get_image', image_id=i, _external=True)
+                  for i in range(len(uploaded_image_data))]
+    return jsonify(image_urls)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,113 @@
+function preview() {
+	const successLoaderDiv = document.getElementById("uploadSuccess");
+	frame.src = URL.createObjectURL(event.target.files[0]);
+	successLoaderDiv.style.display = "none";
+}
+
+function clearImage() {
+	document.getElementById("formFile").value = null;
+	frame.src = "";
+}
+
+function openModal(url) {
+	const modalImg = document.getElementById("modalImage");
+	modalImg.src = url;
+	const imageModal = new bootstrap.Modal(document.getElementById("imageModal"));
+	imageModal.show();
+}
+
+function appendImagesToContainer(imagesContainer, url) {
+	const img = new Image();
+	img.src = url;
+	img.classList.add("shadow");
+	img.classList.add("cursor_pointer");
+	img.addEventListener("click", () => openModal(url));
+	imagesContainer.appendChild(img);
+}
+
+function handleUpload(url) {
+	const loader = document.getElementById("uploadLoader");
+	const successLoader = document.getElementById("uploadSuccessAnim");
+	const successLoaderDiv = document.getElementById("uploadSuccess");
+	const imagesContainer = document.getElementById("imagesContainer");
+
+	loader.style.display = "none";
+	successLoaderDiv.style.display = "block";
+	successLoader.play();
+	clearImage();
+
+	appendImagesToContainer(imagesContainer, url);
+}
+
+function sendImageToServer() {
+	console.log("sending image to server");
+	const fileInput = document.getElementById("formFile");
+	const file = fileInput.files[0];
+
+	if (file) {
+		const loader = document.getElementById("uploadLoader");
+		loader.style.display = "block";
+
+		const formData = new FormData();
+		formData.append("image", file);
+
+		console.log({ file });
+
+		fetch("/api/upload", {
+			method: "POST",
+			body: formData,
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				console.log(data.url);
+				// Handle response from server here
+				handleUpload(data.url);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	} else {
+		alert("Please choose an image first.");
+	}
+}
+
+async function fetchAndDisplayAllImages() {
+	try {
+		// Get the list of image URLs from the /api/images endpoint
+		const response = await fetch("/api/images");
+		const imageUrls = await response.json();
+
+		console.log({ imageUrls });
+
+		const imagesContainer = document.getElementById("imagesContainer");
+
+		// Loop through the image URLs and display each one
+		for (let url of imageUrls) {
+			appendImagesToContainer(imagesContainer, url);
+		}
+	} catch (error) {
+		console.error("Error fetching and displaying images:", error);
+	}
+}
+
+// Call the function to fetch and display images
+fetchAndDisplayAllImages();
+
+function downloadImageHandler() {
+	console.log("download button clicked");
+	const modalImage = document.getElementById("modalImage");
+	const imageUrl = modalImage.src;
+	const imageName = imageUrl.split("/").pop(); // This will extract the image name from the URL. Modify as needed.
+
+	downloadImage(imageUrl, imageName);
+}
+
+function downloadImage(url, name) {
+	const anchor = document.createElement("a");
+	anchor.href = url;
+	anchor.download = name;
+	document.body.appendChild(anchor);
+	anchor.click();
+	document.body.removeChild(anchor);
+}
